00e64f3865264991601397f4d1c0612bcfd388b4
Story5 <337995545@qq.com>
2017-04-06 11:33:05 +0800

    single view application

---
+++ AVFoundationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVFoundationDemo
+//
+//  Created by Story5 on 4/6/17.
+//  Copyright © 2017 Story5. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ AVFoundationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVFoundationDemo
+//
+//  Created by Story5 on 4/6/17.
+//  Copyright © 2017 Story5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
