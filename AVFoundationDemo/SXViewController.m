3f0ff4e8ffc0eb3f2c1debdf2c45df8f043e0ae7
Story5 <337995545@qq.com>
2017-04-07 11:19:59 +0800

    config AVCapture

--- AVFoundationDemo/SXViewController.m
+++ AVFoundationDemo/SXViewController.m
@@ -25,6 +25,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setAVCapture];
 }
 
 
@@ -33,30 +34,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate
+// output
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    // Create a UIImage from the sample buffer data
+    UIImage *image = [self imageFromSampleBuffer:sampleBuffer];
+    
+    // Add your code here that uses the image
+//    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    NSLog(@"image");
+}
+
+// drop
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didDropSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    
+}
+
 #pragma mark - set AVCapture
 - (void)setAVCapture
 {
     NSError *error = nil;
     
-    // Create the session
+    /*  **********   步骤 - 1   **********
+     *  Create the session
+     */
     self.session = [[AVCaptureSession alloc] init];
-    
     // Configure the session to produce lower resolution video frames, if your
     // processing algorithm can cope. We'll specify medium quality for the
     // chosen device.
     self.session.sessionPreset = AVCaptureSessionPresetMedium;
     
-    // Find a suitable AVCaptureDevice
+    /*  **********   步骤 - 2   **********
+     *  Find a suitable AVCaptureDevice
+     */
     self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
-    // Create a device input with the device and add it to the session.
+    /*  **********   步骤 - 3   **********
+     *  Create a device input with the device and add it to the session
+     */
     self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:&error];
     if (!self.input) {
         // Handling the error appropriately.
     }
     [self.session addInput:self.input];
     
-    // Create a VideoDataOutput and add it to the session
+    /*  **********   步骤 - 4   **********
+     *  Create a VideoDataOutput and add it to the session
+     */
     self.output = [[AVCaptureVideoDataOutput alloc] init];
     [self.session addOutput:self.output];
     
@@ -65,22 +91,88 @@
     [self.output setSampleBufferDelegate:self queue:queue];
     
     // Specify the pixel format
-    self.output.videoSettings =
-    [NSDictionary dictionaryWithObject:
-     [NSNumber numberWithInt:kCVPixelFormatType_32BGRA]
-                                forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+    self.output.videoSettings = [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA]
+                                                            forKey:(id)kCVPixelBufferPixelFormatTypeKey];
     
     
     // If you wish to cap the frame rate to a known value, such as 15 fps, set
     // minFrameDuration.
-    self.output.minFrameDuration = CMTimeMake(1, 15);
+//    self.output.minFrameDuration = CMTimeMake(1, 15);
+    [self.device lockForConfiguration:&error];
+    self.device.activeVideoMinFrameDuration = CMTimeMake(1, 15);
     
-    // Start the session running to start the flow of data
+    /*  **********   步骤 - 5   **********
+     * Start the session running to start the flow of data
+     */
     [self.session startRunning];
     
     // Assign session to an ivar.
     [self setSession:self.session];
     
+}
+
+// Create a UIImage from sample buffer data
+- (UIImage *)imageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer
+{
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    // Lock the base address of the pixel buffer
+    CVPixelBufferLockBaseAddress(imageBuffer,0);
+    
+    // Get the number of bytes per row for the pixel buffer
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    // Get the pixel buffer width and height
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    
+    // Create a device-dependent RGB color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    if (!colorSpace)
+    {
+        NSLog(@"CGColorSpaceCreateDeviceRGB failure");
+        return nil;
+    }
+    
+    // Get the base address of the pixel buffer
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    // Get the data size for contiguous planes of the pixel buffer.
+    size_t bufferSize = CVPixelBufferGetDataSize(imageBuffer);
+    
+    // Create a Quartz direct-access data provider that uses data we supply
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, baseAddress, bufferSize,
+                                                              NULL);
+    // Create a bitmap image from data supplied by our data provider
+    CGImageRef cgImage =
+    CGImageCreate(width,
+                  height,
+                  8,
+                  32,
+                  bytesPerRow,
+                  colorSpace,
+                  kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Little,
+                  provider,
+                  NULL,
+                  true,
+                  kCGRenderingIntentDefault);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    // Create and return an image object representing the specified Quartz image
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+    
+    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+    
+    return image;
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Tips" message:@"save image failure" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alert addAction:cancel];
+    [self presentViewController:alert animated:true completion:nil];
 }
 
 
